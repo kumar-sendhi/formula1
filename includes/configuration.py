02bbac54a7a1f41352235e6763d96e8e1993e993
kumar.sendhi@outlook.com <kumar.sendhi@outlook.com>
2022-12-18 20:50:44 +0000

    create race_results tables in presentation schema

--- includes/configuration.py
+++ includes/configuration.py
@@ -1,2 +1,4 @@
 # Databricks notebook source
 raw_folder_path = "abfss://raw@formula1seyondl2.dfs.core.windows.net/"
+presentation_folder_path = "abfss://presentation@externaltableseyon.dfs.core.windows.net"
+processed_folder_path = "abfss://processed@externaltableseyon.dfs.core.windows.net"
