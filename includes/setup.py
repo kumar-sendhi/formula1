080ce184285e902f895019aeb7445836896bd993
kumar.sendhi@outlook.com <kumar.sendhi@outlook.com>
2022-12-18 00:40:48 +0000

    ingesting cirucits from raw data and creating external table complete

---
+++ includes/setup.py
@@ -0,0 +1,50 @@
+# Databricks notebook source
+spark.sql("create catalog if not exists formula1")
+
+# COMMAND ----------
+
+spark.sql("USE catalog formula1")
+
+# COMMAND ----------
+
+display(spark.sql("show catalogs"))
+
+# COMMAND ----------
+
+# Grant create and use catalog permissions for the catalog to all users on the account.
+# This also works for other account-level groups and individual users.
+spark.sql("""
+  GRANT CREATE, USE CATALOG
+  ON CATALOG formula1
+  TO `formula1_developers`""")
+
+# COMMAND ----------
+
+# Show grants on the quickstart catalog.
+display(spark.sql("SHOW GRANT ON CATALOG formula1"))
+
+# COMMAND ----------
+
+# Create a schema in the catalog that was set earlier.
+spark.sql("""
+  CREATE SCHEMA IF NOT EXISTS processed
+  COMMENT 'A new Unity Catalog schema called processed'""")
+
+# COMMAND ----------
+
+# Show schemas in the catalog that was set earlier.
+display(spark.sql("SHOW SCHEMAS"))
+
+# COMMAND ----------
+
+# Describe the schema.
+display(spark.sql("DESCRIBE SCHEMA EXTENDED processed"))
+
+# COMMAND ----------
+
+# Grant create table, create view, and use schema permissions for the schema to all users on the account.
+# This also works for other account-level groups and individual users.
+spark.sql("""
+  GRANT CREATE TABLE, CREATE VIEW, USE SCHEMA
+  ON SCHEMA processed
+  TO `formula1_developers`""")
--- ingestion/1.ingest_circuits_file.py
+++ ingestion/1.ingest_circuits_file.py
@@ -1,2 +1,130 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest circuits.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 1 - Read the csv file using the spark dataframe reader
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_data_source","")
+v_data_source =dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date","2021-03-21")
+v_file_date =dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# Set the current catalog.
+spark.sql("USE CATALOG formula1")
+
+# COMMAND ----------
+
+# Set the current schema.
+spark.sql("USE processed")
+
+# COMMAND ----------
+
+# Show the current database (also called a schema).
+spark.catalog.currentDatabase()
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+from pyspark.sql.functions import col, current_timestamp,lit
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields =[StructField("circuitId",IntegerType(),False),
+                                     StructField("circuitRef",StringType(),True),
+                                     StructField("name",StringType(),True),
+                                     StructField("location",StringType(),True),
+                                     StructField("country",StringType(),True),
+                                     StructField("lat",DoubleType(),True),
+                                     StructField("lng",DoubleType(),True),
+                                     StructField("alt",IntegerType(),True),
+                                     StructField("url",StringType(),True)
+                                     ])
+
+# COMMAND ----------
+
+circuits_df = spark.read.option("header", "true") \
+.schema(circuits_schema) \
+.csv(f'{raw_folder_path}/{v_file_date}/circuits.csv')
+
+# COMMAND ----------
+
+type(circuits_df)
+
+# COMMAND ----------
+
+circuits_df.show()
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select('circuitId','circuitRef','name','location','country','lat','lng','alt')
+
+# COMMAND ----------
+
+display(circuits_selected_df)
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId","circuit_id") \
+.withColumnRenamed("circuitRef","circuit_ref") \
+.withColumnRenamed("name","circuits_name") \
+.withColumnRenamed("lat","latitude") \
+.withColumnRenamed("lng","longitude") \
+.withColumnRenamed("alt","altitude") 
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn("env",lit("Production")) \
+.withColumn("data_source",lit(v_data_source)) \
+.withColumn("file_date",lit(v_file_date))
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").format("delta").option("path","abfss://processed@externaltableseyon.dfs.core.windows.net/circuits").saveAsTable("circuits_um")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESCRIBE TABLE extended circuits_um;
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").format("delta").saveAsTable("circuits")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESCRIBE TABLE extended circuits;
+
+# COMMAND ----------
+
+spark.sql("drop table if exists circuits")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 
